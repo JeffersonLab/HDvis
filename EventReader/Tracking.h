6cbf493c02621c5c0bd1ce239fd0924d9a939e1f
T-Britton <tbritton@jlab.org>
2018-04-03 15:43:54 -0400

    More snapTos with camera facing fix

--- EventReader/Tracking.h
+++ EventReader/Tracking.h
@@ -15,7 +15,6 @@
 #define EVESTANDALONE_TRACKING_H
 
 
-
 class Tracking
 {
 public:
@@ -39,12 +38,22 @@ public:
 
         for(int i=0;i<ChargedTracks.size()/*TrackCandidates.size()*/;i++)
         {
-
+            auto jsonHypoTracks = tao::json::value::array({});
             //TAG THE BEST ONE HERE
+            //std::vector<TrackHypo> AllHypos;
+
             string BestPID_name = ParticleType(ChargedTracks[i]->Get_BestTrackingFOM()->PID());
+            string Bestname = BestPID_name + Form(" Track Points %i", i);
+
+            tao::json::value track(
+                    {
+                            {"id", i},
+                            {"BestTrackingPID", Bestname}
+
+                    });
 
             for (int w=0;w<ChargedTracks[i]->dChargedTrackHypotheses.size();w++) {
-
+                //TrackHypo* th= new TrackHypo;
                 string PID_name=ParticleType(ChargedTracks[i]->dChargedTrackHypotheses[w]->PID());
 
                 string name = PID_name + Form(" Track Points %i", i);
@@ -96,12 +105,15 @@ public:
 
                 // event_out<<WriteTrackJSON2(name, momentum,charge,TrackChiSq_NDF, track_points);
                 // std::cout<<name<<" , "<< momentum.Mag()<<" , "<<charge<<" , "<<TrackChiSq_NDF<<" , "<<start_time<<" , "<< track_points.size()<<" , "<< track_point_times.size()<<std::endl;
-                jsonTracks.emplace_back(WriteTrackJSON(name, momentum, charge, TrackChiSq_NDF, start_time, track_points,
-                                                       track_point_times, mass, position));
+                jsonHypoTracks.emplace_back(WriteTrackJSON(name, momentum, charge, TrackChiSq_NDF, start_time, track_points,
+                                                           track_point_times, mass, position));
                 track_points.clear();
                 track_point_times.clear();
                 //delete track_points;
             }
+            track["TrackHypos"] = jsonHypoTracks;
+            jsonTracks.emplace_back(track);
+            jsonHypoTracks.reset();
         }
 
 
@@ -113,63 +125,87 @@ public:
     tao::json::value Add_DNeutralParticles(vector<const DNeutralParticle*> NeutralTracks)
     {
         //ostringstream event_out;
-       // event_out<<"\"neutral_tracks\": "<<"[\n";//JSON
-        auto jsonTracks = tao::json::value::array({});
+        // event_out<<"\"neutral_tracks\": "<<"[\n";//JSON
 
-        for(int i=0;i<NeutralTracks.size()/*TrackCandidates.size()*/;i++)
-        {
+        auto jsonnTracks = tao::json::value::array({});
 
-            string PID_name=ParticleType(NeutralTracks[i]->Get_BestFOM()->PID());
-            string name=PID_name + Form(" Track Points %i", i);
-            //cout<<name<<endl;
-            rt->Reset();
+        for(int i=0;i<NeutralTracks.size()/*TrackCandidates.size()*/;i++) {
 
-            rt->SetMass(NeutralTracks[i]->Get_BestFOM()->mass());
-            //rt.SetMass(TrackCandidates[i]->mass());
+            auto jsonnHypoTracks = tao::json::value::array({});
+            //TAG THE BEST ONE HERE
+            //std::vector<TrackHypo> AllHypos;
 
-            auto momentum=NeutralTracks[i]->Get_BestFOM()->momentum();
-            auto position=NeutralTracks[i]->Get_BestFOM()->position();
-            auto mass=NeutralTracks[i]->Get_BestFOM()->mass();
-            auto start_time=-1E9;
-            if(!isnan(float(NeutralTracks[i]->Get_BestFOM()->t0())))
-            {
-                start_time = NeutralTracks[i]->Get_BestFOM()->t0();
+            string BestPID_name = ParticleType(NeutralTracks[i]->Get_BestFOM()->PID());
+            string Bestname = BestPID_name + Form(" Track Points %i", i);
+
+            tao::json::value ntrack(
+                    {
+                            {"id",              i},
+                            {"BestTrackingPID", Bestname}
+
+                    });
+
+            for (int w = 0; w < NeutralTracks[i]->dNeutralParticleHypotheses.size(); w++) {
+
+                string PID_name = ParticleType(NeutralTracks[i]->dNeutralParticleHypotheses[w]->PID());
+                string name = PID_name + Form(" Track Points %i", i);
+                //cout<<name<<endl;
+                rt->Reset();
+
+                rt->SetMass(NeutralTracks[i]->dNeutralParticleHypotheses[w]->mass());
+                //rt.SetMass(TrackCandidates[i]->mass());
+
+                auto momentum = NeutralTracks[i]->dNeutralParticleHypotheses[w]->momentum();
+                auto position = NeutralTracks[i]->dNeutralParticleHypotheses[w]->position();
+                auto mass = NeutralTracks[i]->dNeutralParticleHypotheses[w]->mass();
+                auto start_time = -1E9;
+                if (!isnan(float(NeutralTracks[i]->dNeutralParticleHypotheses[w]->t0()))) {
+                    start_time = NeutralTracks[i]->dNeutralParticleHypotheses[w]->t0();
+                }
+
+                rt->Swim(NeutralTracks[i]->dNeutralParticleHypotheses[w]->position(), momentum,
+                         NeutralTracks[i]->dNeutralParticleHypotheses[w]->charge());
+                //rt.Swim(TrackCandidates[i]->position(), TrackCandidates[i]->momentum(), TrackCandidates[i]->charge());
+                DReferenceTrajectory::swim_step_t *steps = rt->swim_steps;
+
+                vector<DVector3> track_points;
+                vector<double> track_point_times;
+
+                for (int j = 0; j < rt->Nswim_steps; j++) {
+                    DVector3 step_loc = steps[j].origin;
+                    //cout<<i<<"|"<<step_loc.X()<<","<<step_loc.Y()<<","<<step_loc.Z()<<endl;
+                    if (step_loc.Z() > 645/*625*/ )
+                        break;
+
+                    if (step_loc.Z() <= 0)
+                        continue;
+
+                    track_points.push_back(step_loc);
+                    track_point_times.push_back(steps[j].t);
+                }
+                //event_out<<tao::json::to_string(WriteTrackJSON2(name,momentum, 0,-1, track_points), 4);
+
+                //std::cout<<name<<" , "<< momentum.Mag()<<" , "<<0<<" , "<<-1<<" , "<<start_time<<" , "<< track_points.size()<<" , "<< track_point_times.size()<<std::endl;
+
+
+
+                jsonnHypoTracks.emplace_back(
+                        WriteTrackJSON(name, momentum, 0, -1, start_time, track_points, track_point_times, mass,
+                                       position));
+                track_points.clear();
+                track_point_times.clear();
             }
-
-            rt->Swim(NeutralTracks[i]->Get_BestFOM()->position(), momentum, NeutralTracks[i]->Get_BestFOM()->charge());
-            //rt.Swim(TrackCandidates[i]->position(), TrackCandidates[i]->momentum(), TrackCandidates[i]->charge());
-            DReferenceTrajectory::swim_step_t* steps =rt->swim_steps;
-
-            vector<DVector3> track_points;
-            vector<double> track_point_times;
-
-            for(int j=0; j<rt->Nswim_steps; j++)
-            {
-                DVector3 step_loc=steps[j].origin;
-                //cout<<i<<"|"<<step_loc.X()<<","<<step_loc.Y()<<","<<step_loc.Z()<<endl;
-                if(step_loc.Z()>645/*625*/ )
-                    break;
-
-                if(step_loc.Z()<=0)
-                    continue;
-
-                track_points.push_back(step_loc);
-                track_point_times.push_back(steps[j].t);
-            }
-            //event_out<<tao::json::to_string(WriteTrackJSON2(name,momentum, 0,-1, track_points), 4);
-
-            //std::cout<<name<<" , "<< momentum.Mag()<<" , "<<0<<" , "<<-1<<" , "<<start_time<<" , "<< track_points.size()<<" , "<< track_point_times.size()<<std::endl;
-            jsonTracks.emplace_back(WriteTrackJSON(name,momentum, 0,-1, start_time,track_points,track_point_times,mass,position));
-            track_points.clear();
-            track_point_times.clear();
+            ntrack["TrackHypos"] = jsonnHypoTracks;
+            jsonnTracks.emplace_back(ntrack);
+            jsonnHypoTracks.reset();
         }
 
-
-        return jsonTracks;
+        return jsonnTracks;
 
     }
     tao::json::value WriteTrackJSON( string id, TVector3 momentum, double charge, double TrackChiSq_NDF, double start_time, vector<DVector3> track_points, vector<double> track_point_times, double mass,TVector3 position)
     {
+
         tao::json::value track(
                 {
                         {"id", id},
@@ -187,8 +223,8 @@ public:
 
         auto jsonposArray = tao::json::value::array({});
 
-            //std::cout<<track_points[j].X()<<" , "<< track_points[j].Y()<<" , "<<track_points[j].Z()<<" , "<<track_point_times[j]<<std::endl;
-            jsonposArray.emplace_back(tao::json::value::array({position.X() , position.Y() , position.Z()}));
+        //std::cout<<track_points[j].X()<<" , "<< track_points[j].Y()<<" , "<<track_points[j].Z()<<" , "<<track_point_times[j]<<std::endl;
+        jsonposArray.emplace_back(tao::json::value::array({position.X() , position.Y() , position.Z()}));
 
 
         track["position"] = jsonposArray;
